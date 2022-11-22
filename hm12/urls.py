4c2b64e747aab4fbb305cf1d2c6173883408f766
stas <stas37951@gmail.com>
2022-11-22 22:56:50 +0200

    del some templates and path

--- hm12/urls.py
+++ hm12/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
-from hm12.views import main_page, get_books, get_author, book, author, get_publisher, get_store, stores, publisher
+from hm12.views import main_page, get_books, get_author, book, author, get_publisher, get_store, stores, publisher, \
+    CreatePublisher, UpdatePublisher, DeletePublisher, DetailPublisher, ListPublisher
 
 app_name = 'hm12'
 urlpatterns = [
@@ -12,6 +13,10 @@ urlpatterns = [
     path('publisher/', get_publisher, name='get_publisher'),
     path('publisher/<int:pk>/', publisher, name='publisher'),
     path('store/', get_store, name='get_store'),
-    path('store/<int:pk>/', stores, name='store')
-
+    path('store/<int:pk>/', stores, name='store'),
+    path('publishers/', CreatePublisher.as_view(), name='CreatePublisher'),
+    path('update_publisher/<int:pk>', UpdatePublisher.as_view(), name='UpdatePublisher'),
+    path('delete_publisher/<int:pk>', DeletePublisher.as_view(), name='DeletePublisher'),
+    path('detail_publisher/<int:pk>', DetailPublisher.as_view(), name='DetailPublisher'),
+    path('list_publisher/', ListPublisher.as_view(), name='ListPublisher')
 ]
--- hm12/views.py
+++ hm12/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse_lazy
+from django.views import generic
+
 from .models import Book, Author, Publisher, Store
 
 
@@ -50,3 +53,37 @@ def stores(request, pk):
     store = get_object_or_404(Store, pk=pk)
     books = store.books.all()
     return render(request, 'store.html', {'store': store, 'books': books})
+
+
+class CreatePublisher(generic.CreateView):
+    template_name = 'publisher_form.html'
+    model = Publisher
+    fields = ['name']
+
+
+class UpdatePublisher(generic.UpdateView):
+    template_name = 'update_publisher.html'
+    model = Publisher
+    fields = ['name']
+
+
+class DeletePublisher(generic.DeleteView):
+    template_name = 'delete_publisher.html'
+    model = Publisher
+    success_url = reverse_lazy('hm12:get_publisher')
+
+
+class DetailPublisher(generic.DetailView):
+    template_name = 'detail_publisher.html'
+    model = Publisher
+    context_object_name = 'publisher'
+
+
+class ListPublisher(generic.ListView):
+    template_name = 'list_publisher.html'
+    model = Publisher
+    paginate_by = 5
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
